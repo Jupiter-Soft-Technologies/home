7dbb56dfddc128dc9da3d33d1d886f9d5f6139bf
pkn9015 <233895766+sonamnishad123@users.noreply.github.com>
2025-11-22 18:36:32 +0530

    u

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Footer() {
   return (
     <footer className="bg-gray-900 text-gray-400 py-6 text-center">
-      <p>© {new Date().getFullYear()} JupitorSoft. All rights reserved.</p>
+      <p>© {new Date().getFullYear()} JupitorSoftTechnologies. All rights reserved.</p>
     </footer>
   );
 }
